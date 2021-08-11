0a0a818188ed0e3119596e9b9555a1e5ab7a4562
maureendss <maureen.deseyssel@gmail.com>
2021-08-11 19:19:35 +0200

    added script to run bilingual infsim experiments

--- trainers/train_cpc_small_inftrain.sh
+++ trainers/train_cpc_small_inftrain.sh
@@ -8,14 +8,17 @@
 #SBATCH --hint=nomultithread          # hyperthreading desactive
 #SBATCH --exclusive
 
+
 # Across-machines training
 export MASTER=`hostname`
 export MASTER_PORT=13369
 export NCCL_DEBUG=INFO # for debugging
 
 PATH_DB=$1
+PATH_CPT=$2
+NB_EPOCHS=$3
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 3 ]; then
   echo "Illegal number of parameters. Please respect :"
   echo "./train_cpc_small.sh /path/to/database/containing/wav/files"
   echo "Example :"
@@ -23,28 +26,6 @@ if [ "$#" -ne 1 ]; then
   exit
 fi
 
-SHARE=$(basename $PATH_DB)
-SIZE=$(basename $(dirname $PATH_DB))
-LANGUAGE=$(basename $(dirname $(dirname $PATH_DB)))
-PATH_CPT=/gpfsssd/scratch/rech/ank/ucv88ce/projects/MultilingualCPC/checkpoints/inftrain/${LANGUAGE}/${SIZE}/${SHARE}/cpc_small
-
-# This can be changed if we observe that models take too much time to train.
-# However, if one decide to lower the number of epochs,
-# one should check that models are indeed converging
-if [ "$SIZE" == "50h" ]; then
-  NB_EPOCHS=200
-elif [ "$SIZE" == "100h" ]; then
-  # 100 epochs for a 100h training set seems fine to me
-  NB_EPOCHS=100
-elif [ "$SIZE" == "200h" ]; then
-  NB_EPOCHS=80
-elif [ "$SIZE" == "400h" ]; then
-  NB_EPOCHS=50;
-else
-  echo "Not possible to deduce the number of epochs from the size of the training set."
-  echo "You should check that you haven't called train_cpc_small.sh with a training set whose size is greater or equal than 800h"
-  exit
-fi;
 
 if [ -f ${PATH_CPT}/running.state ]; then
   echo "${PATH_CPT}/running.state found. Not running anything."
@@ -55,16 +36,17 @@ touch ${PATH_CPT}/running.state
 
 echo "Start training $PATH_CPT"
 module load sox
+# source activate cpc2
+#source activate multicpc
 source activate inftrain
 
 HOME=/gpfsdswork/projects/rech/ank/ucv88ce/
 export PYTHONPATH=$HOME/repos/CPC_torch:$HOME/projects/MultilingualCPC/WavAugment:$PYTHONPATH
 
-
 srun python /gpfsdswork/projects/rech/ank/ucv88ce/repos/CPC_torch/cpc/train.py --pathCheckpoint ${PATH_CPT} \
-                           --pathDB ${PATH_DB} --restart \
+     --pathDB ${PATH_DB} --restart \
                            --file_extension .wav --nLevelsGRU 2 --save_step 2 --multihead_rnn --restart \
-                           --nEpoch ${NB_EPOCHS} --random_seed 42 --n_process_loader 1 --save_step 5 \
+                           --nEpoch ${NB_EPOCHS} --random_seed 42 --n_process_loader 1 \
                            --distributed --master_port $MASTER_PORT
 
 
